1990f7c032ee540ccebdb8cb7f347b9418f4a297
Dmitry Azov <D@azov93.ru>
2022-02-20 17:37:33 +0300

    add function

--- lesson1/index.js
+++ lesson1/index.js
@@ -1,6 +1,26 @@
-console.log('я вывел')
+/* console.log('я вывел')
 
 let prent = prompt('сколько тебе лет?');
-console.log(prent);
+console.log(prent); */
+
+'use strict';
 
 
+
+function userF (userName,yes,no) {
+   if (confirm(userName)){
+       yes();
+   }else{
+       no()
+   }
+}
+
+function showOK() {
+    alert('проходи');
+}
+
+function showNo() {
+    alert('уходи');
+}
+
+userF('тебе есть 18?', showOK, showNo);
