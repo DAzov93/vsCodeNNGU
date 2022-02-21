e2a5b4e5dc93640e95990b187a9d80139c67d895
Dmitry Azov <D@azov93.ru>
2022-02-22 01:15:07 +0300

    add home Work

---
+++ lesson1/Home_work/homeWork.js
@@ -0,0 +1,23 @@
+
+const ourPlanetName = "ourPlanetName Переменная для “названия нашей планеты” ";
+
+let userCurrentTime = 'userCurrentTime Переменная для “текущего время пользователя”';
+
+let numberArticles = 'numberArticles Переменная которая показывает “количества статей”';
+
+let paymentMethod = 'paymentMethod Переменная которая показывает “это оплата наличными деньгами или картой”';
+
+let firstName = 'firstName  имя пользователя';
+
+let lastName = 'lastName фамилия пользователя';
+
+let patronymic = 'patronymic отчество пользователя';
+
+console.log(ourPlanetName);
+console.log(userCurrentTime);
+console.log(numberArticles);
+console.log(paymentMethod);
+console.log(firstName);
+console.log(lastName);
+console.log(patronymic);
+
